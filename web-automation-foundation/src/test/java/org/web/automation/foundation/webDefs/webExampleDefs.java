23509985b975a561cfaff061d444949b1577d8c0
Diogo Bettencourt <diogobettencourt97@gmail.com>
2021-10-21 12:08:44 +0000

    Update: Example Defs and Feature File

--- web-automation-foundation/src/test/java/org/web/automation/foundation/webDefs/webExampleDefs.java
+++ web-automation-foundation/src/test/java/org/web/automation/foundation/webDefs/webExampleDefs.java
@@ -3,7 +3,7 @@ package org.web.automation.foundation.webDefs;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.web.automation.foundation.utilityClasses.WebUtility;
@@ -14,19 +14,51 @@ public class webExampleDefs {
     private final WebDriver driver = WebUtility.getDriver();
     private final WebDriverWait wait = WebUtility.getWait();
 
-    @Given("^I am in this Website - Define Context$")
+    @Given("I am in this Example Website")
     public void something1() {
         // Cucumber Given Method Implementation
+
+        // Go to the Example Website
+        driver.get("https://demoqa.com/");
     }
 
-    @When("^I do this in the Website - Action / Input$")
+    @When("I go to the Example Form")
     public void something2() {
         // Cucumber When Method Implementation
+
+        // Click "Elements" Option
+        WebUtility.findElementByText("Elements").click();
+        WebUtility.validatePageUrl("elements");
+
+        // Click "Text Box" Option
+        WebUtility.findElementByText("Text Box").click();
+        WebUtility.validatePageUrl("text-box");
     }
 
-    @Then("^This should happen - Result / Output$")
-    public void something3() {
+    @When("I fill the Example Form with {string} Name and {string} Email")
+    public void something3(String name, String email) {
+        // Cucumber When Method Implementation
+
+        // Write Full Name and Email
+        driver.findElement(By.id("userName")).sendKeys(name);
+        driver.findElement(By.id("userEmail")).sendKeys(email);
+    }
+
+    @Then("I submit the Example Form")
+    public void something4() {
         // Cucumber Then Method Implementation
+
+        // Click "Submit" Button
+        driver.findElement(By.id("submit")).click();
+    }
+
+    @Then("I validate the Example Form")
+    public void something5() {
+        // Cucumber Then Method Implementation
+
+        // Validate Example Form Elements Text
+        WebUtility.validateElementTextContains(driver.findElement(By.id("name")), "Name:");
+        WebUtility.validateElementTextContains(driver.findElement(By.id("email")), "Email:");
     }
 
 }
--- web-automation-foundation/src/test/resources/org/web/automation/foundation/webFeatures/login.feature
+++ web-automation-foundation/src/test/resources/org/web/automation/foundation/webFeatures/login.feature
@@ -3,7 +3,9 @@ Feature: Web [Insert Feature Name Here]
   -> Do Something
   -> Do Something
 
-  Scenario: [Scenario Name] Example: Successful Login
-    Given I am in this Website - Define Context
-    When I do this in the Website - Action / Input
-    Then This should happen - Result / Output
+  Scenario: [Scenario Name] Example: Fill Address Form
+    Given I am in this Example Website
+    When I go to the Example Form
+    And I fill the Example Form with "Example" Name and "example@email.com" Email
+    Then I submit the Example Form
+    And I validate the Example Form
