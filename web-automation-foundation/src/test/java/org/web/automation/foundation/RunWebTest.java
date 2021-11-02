618e4872453aa0c412c8a62b6762ff99b3257c57
Diogo Bettencourt <diogobettencourt97@gmail.com>
2021-11-02 11:28:32 -0100

    Update: Cucumber Report Phase

--- web-automation-foundation/src/test/java/org/web/automation/foundation/RunWebTest.java
+++ web-automation-foundation/src/test/java/org/web/automation/foundation/RunWebTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/cucumber-report/cucumber.json"},
+@CucumberOptions(plugin = {"json:target/cucumber-report/cucumber.json"},
         features = "src/test/resources/org/web/automation/foundation/webFeatures")
 public class RunWebTest {
     // Run This Cucumber Test Class to Run all the Web Automated Tests
