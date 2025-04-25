a7ab19d82791f4fd47f406ffc8487a23058c6382
Diogo Bettencourt <diogobettencourt97@gmail.com>
2025-04-26 00:26:14 +0100

    Update: Cypress Configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('cypress');
-const { createBundler } = require('@bahmutov/cypress-esbuild-preprocessor');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
 
@@ -13,7 +13,26 @@ async function setupNodeEvents(on, config) {
     })
   );
 
+  setupBaseUrl(config);
   return config;
+}
+
+function setupBaseUrl(config) {
+  const stage = config.env.stage;
+
+  switch (stage) {
+    case 'env1':
+      config.baseUrl = 'https://the-internet.herokuapp.com';
+      break;
+    case 'env2':
+      config.baseUrl = 'https://env2.example.com';
+      break;
+    case 'env3':
+      config.baseUrl = 'https://env3.example.com';
+      break;
+    default:
+      throw new Error(`Unknown stage: ${stage}`);
+  }
 }
 
 module.exports = defineConfig({
@@ -26,61 +45,13 @@ module.exports = defineConfig({
   omitFilteredSpecs: true,
   e2e: {
     env: {},
+    specPattern: '**/*.feature',
     setupNodeEvents,
-    baseUrl: getBaseUrl(),
-    watchForFileChanges: true,
-    specPattern: '**/*.feature'
+    watchForFileChanges: true
   },
   retries: {
     runMode: 1,
     openMode: 0
   },
-  chromeWebSecurity: false,
-  clientCertificates: [
-    {
-      url: 'https://env1.com',
-      ca: [],
-      certs: [
-        {
-          pfx: 'certs/certificate.p12',
-          passphrase: 'certs/password.txt'
-        }
-      ]
-    },
-    {
-      url: 'https://env2.com',
-      ca: [],
-      certs: [
-        {
-          pfx: 'certs/certificate.p12',
-          passphrase: 'certs/password.txt'
-        }
-      ]
-    },
-    {
-      url: 'https://env3.com',
-      ca: [],
-      certs: [
-        {
-          pfx: 'certs/certificate.p12',
-          passphrase: 'certs/password.txt'
-        }
-      ]
-    }
-  ]
+  chromeWebSecurity: false
 });
-
-function getBaseUrl() {
-  const stage = process.env.CYPRESS_stage;
-
-  switch (stage) {
-    case 'env1':
-      return 'https://the-internet.herokuapp.com';
-    case 'env2':
-      return 'https://env2.example.com';
-    case 'env3':
-      return 'https://env3.example.com';
-    default:
-      throw new Error(`Unknown stage: ${stage}`);
-  }
-}
